860c88d051645914977f596a9dbf7aacd7016f04
Oktay Karakaş <oktaykarakas71@gmail.com>
2024-10-06 00:58:55 +0300

    items sorted

--- main.py
+++ main.py
@@ -2,22 +2,34 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_content = f.read()
         print(f"number of words : {count_words(file_content)}")
-        count_chars_dict = count_chars(file_content)
-        for key in count_chars_dict:
-            if key.isalpha():
-                print(f"number of {key} occurance : {count_chars_dict[key]}")
+        count_chars_dict_list = count_chars(file_content)
+        print(count_chars_dict_list)
+        for dict in count_chars_dict_list:
+            if dict["char"].isalpha():
+                print(f"number of {dict['char']} occurance : {dict['num']}")
 
 def count_words(content):
     words = content.split()
     return len(words)
 
+def sort_on(dict):
+    return dict["num"]
+
 def count_chars(content):
+
     chars = {}
+    chars_list = []
+
     for char in content:
         if not char.lower() in chars:
             chars[char.lower()] = 1
         else:
             chars[char.lower()] += 1
-    return chars
+        
+    for key in chars:
+        chars_list.append({"char":key,"num":chars[key]})
+
+    chars_list.sort(reverse=True,key=sort_on)
+    return chars_list
 
 main()
